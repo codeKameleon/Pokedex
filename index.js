f5fba134e85170ce6254b92e562943fcc6cab68c
axel <axel@pop-os.localdomain>
2021-11-10 15:29:19 +0100

    improving design of info screen + creating capitalizeFirstLetter function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const search_pokedex = document.querySelector('.search-pokedex')
 const info_screen_pokedex =  document.querySelector('.info-screen')
+const pokemon_image_container =  document.querySelector('.pokemon-image-container')
 
 search_pokedex.addEventListener('submit', e => {
     const search_value = document.querySelector('.search-control').value
     e.preventDefault()
     getPokemon(search_value)
 })
+
+const capitalizeFirstLetter = str => {
+    return str.charAt(0).toUpperCase()  + str.slice(1)
+}
 
 const getPokemon = async (id, name) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id || name}`
@@ -17,32 +22,50 @@ const getPokemon = async (id, name) => {
 const renderPokemonCard = pokemon => {
     const { id, name, sprites, moves } = pokemon
 
+    info_screen_pokedex.innerHTML = ''
+    pokemon_image_container.innerHTML = ''
+
     const pokemon_card = document.createElement('div')
     pokemon_card.className = 'pokemon-card'
 
+    // ID
     const pokemon_id = document.createElement('span')
-    pokemon_id.textContent = id
+    pokemon_id.textContent = `ID :  ${id}`
 
+    // NAME
     const pokemon_name = document.createElement('span')
-    pokemon_name.textContent = name
+    pokemon_name.textContent = `NAME :  ${capitalizeFirstLetter(name)}`
 
+    // SPRITE
     const pokemon_sprite = document.createElement('img')
     pokemon_sprite.src = sprites.front_default
 
-    const pokemon_moves = document.createElement('ul')
-    pokemon_moves.className = 'pokemon-moves'
+    // MOVES
+    const pokemon_moves_container = document.createElement('div')
+
+    const pokemon_moves_title =  document.createElement('span')
+    pokemon_moves_title.textContent =  'MOVES : '
+
+    const pokemon_moves_list = document.createElement('ul')
+    pokemon_moves_list.className = 'pokemon-moves'
+    
     const basic_moves = moves.slice(0,4)
     basic_moves.forEach(basic_move => {
-        console.log()
         const move = document.createElement('li')
         move.textContent = basic_move.move.name
-        pokemon_moves.appendChild(move)
+        pokemon_moves_list.appendChild(move)
     })
 
+    pokemon_moves_container.appendChild(pokemon_moves_title)
+    pokemon_moves_container.appendChild(pokemon_moves_list)
+
+    // Pokemon sprite (left screen)
+    pokemon_image_container.appendChild(pokemon_sprite)
+
+    // Data relatives to the pokemon(right screen)
     pokemon_card.appendChild(pokemon_id)
     pokemon_card.appendChild(pokemon_name)
-    pokemon_card.appendChild(pokemon_sprite)
-    pokemon_card.appendChild(pokemon_moves)
+    pokemon_card.appendChild(pokemon_moves_container)
 
 
     const getPokemonPreviousEvolution = async () => {
@@ -50,8 +73,6 @@ const renderPokemonCard = pokemon => {
         const response = await fetch(url)
         const pokemon_species = await response.json()
         const previous_evolution = pokemon_species.evolves_from_species
-
-        console.log(pokemon_species)
 
         if(previous_evolution !== null) {
             const url = `https://pokeapi.co/api/v2/pokemon/${previous_evolution.name}`
@@ -70,16 +91,20 @@ const renderPokemonCard = pokemon => {
 
 const renderPreviousEvolution = (pokemon_card, previous_evolution) => {
     const evolution_container = document.createElement('div')
+    evolution_container.className =  'evolution-container'
 
     const evolution_title =  document.createElement('span')
     evolution_title.className =  'evolution-title'
-    evolution_title.textContent = 'Evolved from'
+    evolution_title.textContent = 'Evolves from'
 
-    evolution_container.className =  'evolution-container'
+    // NAME
     const previous_evolution_name = document.createElement('span')
-    previous_evolution_name.textContent = previous_evolution.name
+    previous_evolution_name.className =  'previous-evolution-name'
+    previous_evolution_name.textContent = capitalizeFirstLetter(previous_evolution.name)
 
+    // SPRITE
     const previous_evolution_sprite = document.createElement('img')
+    previous_evolution_sprite.className =  'previous-evolution-sprite'
     previous_evolution_sprite.src = previous_evolution.sprites.front_default
 
     evolution_container.appendChild(evolution_title)
